667e47b20ae3fc52865a29bf7c893224e4b50e31
user <user@LAPTOP-Q7AL5D2T>
2024-07-24 21:29:54 +0530

    corrected AdminUserPage

--- com.sevenrmartsupermarket/src/main/java/com/sevenrmartsupermarket/pages/AdminUserPage.java
+++ com.sevenrmartsupermarket/src/main/java/com/sevenrmartsupermarket/pages/AdminUserPage.java
@@ -8,127 +8,113 @@ import org.openqa.selenium.support.PageFactory;
 import com.sevenrmartsupermarket.utilities.GeneralUtility;
 import com.sevenrmartsupermarket.utilities.PageUtility;
 
-public class AdminUserPage  {
+public class AdminUserPage {
 
 	public WebDriver driver;
-	
-	
-	@FindBy(xpath="//div[@class='small-box bg-info']//p[contains(text(),'Admin')]")
-	WebElement adminUserTile;
 
-	@FindBy(xpath="//div[@class='col-sm-6']//h1[contains(text(),'Admin Users')]")
+	@FindBy(xpath = "//div[@class='small-box bg-info']//p[contains(text(),'Admin')]")
+	WebElement adminUserTile;
+	@FindBy(xpath = "//div[@class='col-sm-6']//h1[contains(text(),'Admin Users')]")
 	WebElement adminProfileElement;
-	@FindBy(xpath="//a[@class='btn btn-rounded btn-danger']")
+	@FindBy(xpath = "//a[@class='btn btn-rounded btn-danger']")
 	WebElement admin_NewButton;
-	@FindBy(xpath="//h3[@class='card-title']")
+	@FindBy(xpath = "//h3[@class='card-title']")
 	WebElement admin_NewPageTextField;
-	@FindBy(xpath="//input[@id='username']")
+	@FindBy(xpath = "//input[@id='username']")
 	WebElement createUserNameField;
-	@FindBy(xpath="//input[@id='password']")
+	@FindBy(xpath = "//input[@id='password']")
 	WebElement createPasswordField;
-	@FindBy(xpath="//Select[@id='user_type']")
+	@FindBy(xpath = "//Select[@id='user_type']")
 	WebElement userTypeDropDown;
-	@FindBy(xpath="//button[@name='Create']")
+	@FindBy(xpath = "//button[@name='Create']")
 	WebElement userCreateButton;
-	@FindBy(xpath="//div[@class='alert alert-success alert-dismissible']")
+	@FindBy(xpath = "//div[@class='alert alert-success alert-dismissible']")
 	WebElement userCreateAlert;
 
-	
-	PageUtility pageutility ;
-	
-	public AdminUserPage (WebDriver driver)
-	{
-		this.driver=driver;
+	PageUtility pageutility;
+
+	public AdminUserPage(WebDriver driver) {
+		this.driver = driver;
 		PageFactory.initElements(driver, this);
-		
+
 	}
-	
 
 	public String getProfileName()
 
 	{
 		return adminProfileElement.getText();
 	}
-	
-	public void clickOn_NewUser()
-	{
-		admin_NewButton.click();;
+
+	public void clickOn_NewUser() {
+		admin_NewButton.click();
+		;
 	}
-	
+
 	public String getAdminProfileName()
 
 	{
 		return admin_NewPageTextField.getText();
 	}
-	
-	
-	/**public String NewUserName()
 
-	{
-		return GeneralUtility.getRandomName() + "Admin" ;
-		
-	}**/
+	/**
+	 * public String NewUserName()
+	 * 
+	 * { return GeneralUtility.getRandomName() + "Admin" ;
+	 * 
+	 * }
+	 **/
 
-	/**public String NewPassword()
+	/**
+	 * public String NewPassword()
+	 * 
+	 * { return NewUserName() + "@123"; }
+	 **/
 
-	{
-		return NewUserName() + "@123";
-	}**/
-	
 	public void enterUserName(String userName)
 
 	{
 		createUserNameField.sendKeys(userName);
 	}
-	
+
 	public void enterPassword(String password)
 
 	{
 		createPasswordField.sendKeys(password);
 	}
 
-/**	public void enterNewUserName()
+	/**
+	 * public void enterNewUserName()
+	 * 
+	 * { createUserNameField.sendKeys(NewUserName()); } public void
+	 * enterNewPassword()
+	 * 
+	 * { createPasswordField.sendKeys(NewPassword()); }
+	 **/
 
-	{
-		createUserNameField.sendKeys(NewUserName());
-	}
-	public void enterNewPassword()
-
-	{
-		createPasswordField.sendKeys(NewPassword());
-	}**/
-	
-	public void selectUserType(String userType)
-	{
-		pageutility= new PageUtility(driver);
+	public void selectUserType(String userType) {
+		pageutility = new PageUtility(driver);
 		pageutility.select_ByVisibleText(userTypeDropDown, userType);
 	}
-	
 
-	
-	public void clickOnUserCreate()
-	{
-		 userCreateButton.click();
+	public void clickOnUserCreate() {
+		userCreateButton.click();
 	}
-	
-	public String getAlertTextOnUserCreation()
-	{
+
+	public String getAlertTextOnUserCreation() {
 		return userCreateAlert.getText();
 	}
-	
-	public void createUser(String userName, String password, String userType)
-	{
+
+	public void createUser(String userName, String password, String userType) {
 		enterUserName(userName);
 		enterPassword(password);
 		selectUserType(userType);
 	}
-	/**public void AddAdminUser()
-	{
-		pageutility= new PageUtility(driver);
-		enterNewserName();
-		enterNewPassword();
-		pageutility.select_ByVisibleText(userTypeDropDown, "Admin");
-		 clickOnUserCreate();
-		
-	}**/
+	/**
+	 * public void AddAdminUser() { pageutility= new PageUtility(driver);
+	 * enterNewserName(); enterNewPassword();
+	 * pageutility.select_ByVisibleText(userTypeDropDown, "Admin");
+	 * clickOnUserCreate();
+	 * 
+	 * }
+	 **/
 }
